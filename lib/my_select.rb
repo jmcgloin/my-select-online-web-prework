26251c12feec438d74595dd49d205f4f5ee7f226
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 15:05:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,10 +3,10 @@ def my_select(collection)
   i = 0
   newArray = []
   while i < collection.count do
-    newArray = (yield collection[i], collection[i].even?, newArray)
+    if collection[i].even? then newArray.push(yield collection[i]) end
     i += 1
   end
   newArray
 end
 
-my_select([1,2,3,4,5]) { |item, bool, ar| if bool then ar.push(item) end; ar }
+my_select([1,2,3,4,5]) { |item| item }
