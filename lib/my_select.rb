76d969e15d81af14211fa099c793e01d967985ac
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 14:34:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
-  
+  if collection.empty then return [] end
+  i = 0
+  newArray = []
+  while i < collection.count do
+    newArray = yield collection[i], newArray
+  end
+  newArray
 end
+
+
